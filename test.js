795b886bc88940c4171d574562d974f3c2c9411f
Varun Kushwah <varun.kush3@gmail.com>
2026-04-17 16:01:50 +0530

    This is the new CI/CD pipeline for CA2

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 const r = require("supertest")
 const e = require("./index")
 
-test("health",async()=>{
+
+test("health", async () => {
     const res = await r(e).get("/health")
-    expect(res.text).toBe("OK")
+    expect(res.text).toBe("Everything is OK!!!!")
 })
